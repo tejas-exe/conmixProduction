70b6c0fab8ccafd594aaacef8dd42f99b2065f9c
RateCheckr-DevTeam <147574388+RateCheckr-DevTeam@users.noreply.github.com>
2023-12-15 09:33:04 +0530

    ui changes img

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { Helmet } from "react-helmet";
 import { AboutUs } from "./components/aboutUs/aboutUs";
 import { Header } from "./components/header/header";
 import { Home } from "./components/home/home";
@@ -7,9 +9,46 @@ import Navbar from "./components/navbar/navbar";
 import Footer from "./components/Footer/footer";
 import WhyUs from "./components/whyus/whyus";
 import Services from "./components/serviceProjects/services";
+
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Conmix RMC - Ready Mix Concrete</title>
+        <meta
+          name="description"
+          content="Provide high-quality ready mix concrete to clients."
+        />
+
+        {/* OpenGraph tags for social media */}
+        <meta property="og:title" content="Conmix RMC - Ready Mix Concrete" />
+        <meta
+          property="og:description"
+          content="Provide high-quality ready mix concrete to clients."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="URL_TO_YOUR_SOCIAL_MEDIA_IMAGE" />
+        <meta property="og:url" content="URL_TO_YOUR_WEBSITE" />
+
+        {/* Twitter card tags for social media */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Conmix RMC - Ready Mix Concrete" />
+        <meta
+          name="twitter:description"
+          content="Provide high-quality ready mix concrete to clients."
+        />
+        <meta name="twitter:image" content="URL_TO_YOUR_SOCIAL_MEDIA_IMAGE" />
+
+        {/* Viewport settings */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* Favicon */}
+        <link rel="icon" type="image/png" href="URL_TO_YOUR_FAVICON" />
+
+        {/* Other meta tags as needed */}
+      </Helmet>
+
       <Header />
       <Navbar />
       <Home />
